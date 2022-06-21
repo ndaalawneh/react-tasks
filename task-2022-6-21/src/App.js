51f60466b098ee3242b2b230d1d332244721d43b
ndaalawneh <ndaalawneh@gmail.com>
2022-06-22 01:00:36 +0300

    fix upload

---
+++ task-2022-6-21/src/App.js
@@ -0,0 +1,17 @@
+import Nav from "./nav";
+import './main.css' ;
+ import React, {Component} from "react";
+export default class App extends Component {
+ 
+  
+  render() { 
+    return (  <div className="App">
+     <Nav></Nav>
+     <p> hello world , welcome to my new website </p>
+    </div> );
+  }
+}
+
+
+
+ 
---
+++ task-2022-6-21/src/nav.js
@@ -0,0 +1,22 @@
+function Nav(){
+  
+    return(
+
+ <nav>
+     
+<ul>
+    <li> <a>Home</a></li>
+    <li> <a>about</a></li>
+    <li> <a>contact us</a></li>
+    <li> <a>register</a></li>
+</ul>
+
+
+   </nav>
+
+    );
+
+
+}
+
+export default Nav ;

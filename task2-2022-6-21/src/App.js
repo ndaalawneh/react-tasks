b7a2fecfaa30a9be90d86c0135c7015b639738ef
ndaalawneh <ndaalawneh@gmail.com>
2022-06-22 01:33:29 +0300

    a n

---
+++ task2-2022-6-21/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Nav from './Nav';
+function App() {
+  const logo = require('./logo.svg');
+  return (
+    <div className="App">
+      <Nav />
+      <div class="card-group">
+        <div class="card">
+          <img class="card-img-top" src={require('./abood.jpeg')} alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src={require('./abood.jpeg')} alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src={require('./abood.jpeg')} alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
